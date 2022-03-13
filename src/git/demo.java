c9dbe61fc0c412368dffb27a376476de236e81d3
kiran warkhade <kiranwarkhade6@gmail.com>
2022-03-13 03:26:51 +0000

    first commit...

---
+++ src/git/demo.java
@@ -0,0 +1,15 @@
+package git;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println("sum of a and b is: "+c);
+	}
+
+}
